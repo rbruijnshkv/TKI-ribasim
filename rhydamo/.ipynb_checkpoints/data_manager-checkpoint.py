4df850a07ad21ca20ce4896439dc86c9c642da05
rbruijnshkv <bruijns@hkv.nl>
2023-09-25 23:25:16 +0200

    dynamic lateral inflow embedded in the underlying functions

--- rhydamo/.ipynb_checkpoints/data_manager-checkpoint.py
+++ rhydamo/.ipynb_checkpoints/data_manager-checkpoint.py
@@ -1,4 +1,5 @@
 import os
+import re 
 import pandas as pd
 import geopandas as gpd
 import numpy as np
@@ -215,75 +216,96 @@ class NetworkProcessor:
         return Point(x, y)
     
     
-    def read_boundary_data(self, BC_file_path, boundary_type):
+    def read_boundary_data(self, BC_file_path, starttime, unit = 's'):
         """
         Read boundary conditions data from a text file and create a DataFrame.
 
         This function reads data from a text file that contains boundary conditions
-        and creates a DataFrame with columns representing different attributes.
+        and creates a DataFrame with columns representing different attributes of the 
+        boundary data. The data can be both stationary as well as non-stationary.
+
+        Depending on the timesteps and number of laterals, this can take up several 
+        minutes.
 
         Parameters:
         BC_file_path (str): The path to the text file containing boundary conditions data from D-Hydro.
-
+        boundary_type (str): The boundary types of D-HYDRO. Support currently only 'waterlevelbnd' and 'lateral_discharge'
         Returns:
         pd.DataFrame: A DataFrame containing boundary conditions data organized in columns.
         """
 
         # Read the data from the file
         with open(BC_file_path, "r") as file:
-            data_str = file.read()
+            lines = [line.strip() for line in file if not line.strip().startswith('#')] #skip lines starting with '#'
+        data_str = '\n'.join(lines) # Split the remaining data into individual lines
 
-        # Split the data into individual lines
-        lines = data_str.strip().split('\n')
+        lines = data_str.strip().split('\n') # Split the data into individual lines
 
-        # Find the index where the first [forcing] section starts
-        start_index = 0
-        for idx, line in enumerate(lines):
-            if line.strip().startswith("[forcing]"):
-                start_index = idx
-                break
+        boundary_df = pd.DataFrame(columns=['name', 
+                                            'function', 
+                                            'timeInterpolation', 
+                                            'quantity',
+                                            'unit',
+                                            'static_value'])
 
-        # Create dictionaries to store the data
-        data_dict = {'name': [], 'function': [], 'timeInterpolation': [], 'quantity': [], 'unit': [], 'value': []}
+        boundary_df.loc[0] = [np.nan] * len(boundary_df.columns) #add first row of nans
 
-        # Process the lines from the start_index and extract the data into the dictionaries
-        current_section = None
-        for line in lines[start_index:]:
-            if line.strip().startswith("[forcing]"):
-                current_section = 'forcing'
-            elif '=' in line:
-                key, value = [item.strip() for item in line.split('=')]
-                if current_section == 'forcing':
-                    if key == 'name':
-                        data_dict[key].append(value)
-                    elif key == 'function':
-                        data_dict[key].append(value)
-                    elif key == 'timeInterpolation':
-                        data_dict[key].append(value)
-                    elif key == 'quantity':
-                        data_dict[key].append(value)
-                    elif key == 'unit':
-                        data_dict[key].append(value)
+        float_pattern = r'^\s*[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?\s+[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?\s*$'
+        total_lines = len(lines)
+        ten_percent_increment = total_lines // 10  # Calculate 10% increment for notification purposes
+        count = 0
+        dynamic = False
+        
+        # Loop through each line in the file
+        for i in range(len(lines)):
+            line = lines[i]
+
+            # Calculate the progress percentage
+            if (i + 1) % ten_percent_increment == 0:
+                progress_percentage = (i + 1) / total_lines * 100
+                print(f"Reading the {progress_percentage:.0f}% of the lines")
+
+
+            if len(line.replace(" ", "")) == 0 or ('General' or 'fileVersion' or 'fileType') in line: #skip blanc lines
+                continue
+            elif 'forcing' not in line.replace(" ", "").strip().lower():
+                stripped = [item.strip() for item in line.split('=')]
+                if len(stripped)==2: #find the key and value, if there is a '=' sign
+                    key, value = stripped
+                    boundary_df.loc[count, key] = value
+                elif boundary_df.iloc[-1]['function'] == 'constant' and len(stripped)==1 and not re.match(float_pattern, line): #check if there is only 1 value, which is not a time series
+                    value = line.replace(" ", "")
+                    boundary_df.loc[count, 'static_value'] = float(value)
+                elif re.match(float_pattern, line) and boundary_df.iloc[-1]['function'] == 'timeseries' or pd.isna(boundary_df.iloc[-1]['function']): #add the time series to the df
+                    # Process lines with two floats
+                    time_stamp, time_value = [float(item) for item in line.split()]
+                    boundary_df.loc[count, 'time_stamp'] = time_stamp
+                    boundary_df.loc[count, 'dynamic_value'] = time_value
+
+                    count +=1 #since its a time serie, go to the next row
+                    dynamic = True
+
             else:
-                # Handle the value field separately as it doesn't have a key-value pair format
-                if current_section == 'forcing' and line.strip() != '':
-                    data_dict['value'].append(float(line.strip()))
+                count +=1
 
-        # Fill missing value fields with None
-        data_dict['value'] = data_dict.get('value', []) + [None] * (len(data_dict['name']) - len(data_dict.get('value', [])))
+        print('All lines are read')
+        print()
+        #fill a substantial part of the NaN values, and filter out irrelevant rows and columns
+        columns_to_fill = ['name', 'function', 'timeInterpolation', 'quantity', 'unit']     #fill columns with the last value for easier look up    # Fill NaN values in specified columns with the previous non-NaN value
+        boundary_df[columns_to_fill] = boundary_df[columns_to_fill].fillna(method='ffill')
+        boundary_df = boundary_df.loc[~boundary_df.name.isna()]
+        
+        if dynamic == False:
+            boundary_df = boundary_df[['name', 'function', 'timeInterpolation', 'quantity', 'unit', 'static_value']]
+        else:
+            boundary_df = boundary_df[['name', 'function', 'timeInterpolation', 'quantity', 'unit', 'static_value', 'time_stamp', 'dynamic_value']]
             
-        # Create a DataFrame from the dictionary
-        df = pd.DataFrame(data_dict)
-
-        if boundary_type == 'LevelBoundary':
-            df = df.loc[df.quantity == 'waterlevelbnd']
-            df['geometry'] = df.apply(self.create_point, axis=1)
-
-        elif boundary_type == 'FlowBoundary':
-            df = df.loc[df.quantity == 'lateral_discharge']  
-
-        return df
-    
+            #Convert starttime to datetime, and add the start time to the time columns
+            starttime = pd.to_datetime(starttime)
+            boundary_df['time_stamp'] = starttime + pd.to_timedelta(boundary_df['time_stamp'], unit='s') * 60 #to do: incorporate more elegant way of defining that the timesteps are in minute
+        
+        # return boundary_df_static, boundary_df_dynamic
+        return boundary_df
     
     def embed_laterals(self, his_file_path, nodes, edges, FlowBoundary, offset):
         """
@@ -307,6 +329,8 @@ class NetworkProcessor:
         his_file = xr.open_dataset(his_file_path)
         x_coordinates_laterals = his_file.lateral_geom_node_coordx
         y_coordinates_laterals = his_file.lateral_geom_node_coordy
+        
+        #To do: assign the x and y coordinates more robust by using the lateral_id within his_file.lateral_geom_node_count
         
         #put the laterals in the correct format
         laterals = pd.DataFrame()
@@ -360,11 +384,129 @@ class NetworkProcessor:
         
         # link it with the FlowBoundary df
         flow_boundary_nodes = final_nodes[final_nodes['NAME_ID'].astype(str).str.startswith('lateral')][['geometry', 'node_id']]
-        FlowBoundary['geometry'] = flow_boundary_nodes.geometry.values
-        FlowBoundary['node_id'] = flow_boundary_nodes.node_id.values
+        
+        #first take the unique FlowBoundary rows, as the names are duplicate if its a non stationairy simulation. Add it back together.
+        FlowBoundary_temp = FlowBoundary.drop_duplicates(subset='name')
+        FlowBoundary_temp = FlowBoundary_temp.loc[FlowBoundary_temp['quantity'] == 'lateral_discharge']
+        FlowBoundary_temp['geometry'] = flow_boundary_nodes.geometry.values
+        FlowBoundary_temp['node_id'] = flow_boundary_nodes.node_id.values
+        FlowBoundary_temp = FlowBoundary_temp[['name', 'geometry', 'node_id']]
+        # print(FlowBoundary_temp)
+        #Merge the temporary df in the original df, and use forward filling to fill the NaN places
+        FlowBoundary = FlowBoundary.merge(FlowBoundary_temp, how = 'inner', left_on = 'name', right_on = 'name')
+        FlowBoundary = FlowBoundary.fillna('ffill') #forward fill
+        FlowBoundary[FlowBoundary == 'ffill'] = np.nan #fill with nans where forward fill is not possible
         # FlowBoundary.set_index('node_id', inplace = True)
         # print(flow_boundary_nodes)
         return final_nodes, edges, FlowBoundary
+    
+#         def embed_laterals(self, his_file_path, nodes, edges, FlowBoundary, offset):
+#         """
+#         Embed lateral flow boundary data into the nodes and edges of a hydraulic model.
+
+#         This function takes lateral flow boundary coordinates, a nodes DataFrame, an edges DataFrame,
+#         and an offset value, and updates the nodes and edges DataFrames to include lateral data
+#         in the hydraulic model. The offset value is required, so that basins and laterals do not
+#         overlap. Positive values will be placed north of the basin, and negatives values south.
+
+#         Parameters:
+#         x_coordinates_laterals (list): List of x-coordinates for lateral flow boundaries.
+#         y_coordinates_laterals (list): List of y-coordinates for lateral flow boundaries.
+#         nodes (pd.DataFrame): DataFrame containing node information.
+#         edges (pd.DataFrame): DataFrame containing edge information.
+#         offset (float): Offset value for lateral flow boundary positions. 
+
+#         Returns:
+#         pd.DataFrame, pd.DataFrame: Updated nodes and edges DataFrames with lateral flow boundary information.
+#         """
+#         his_file = xr.open_dataset(his_file_path)
+#         x_coordinates_laterals = his_file.lateral_geom_node_coordx
+#         y_coordinates_laterals = his_file.lateral_geom_node_coordy
+        
+#         #put the laterals in the correct format
+#         laterals = pd.DataFrame()
+#         laterals = gpd.GeoDataFrame(laterals, geometry = gpd.points_from_xy(x_coordinates_laterals, y_coordinates_laterals+offset)) 
+#         laterals['type'] = 'FlowBoundary'
+#         laterals['node_id'] = np.arange(max(nodes.node_id)+1, len(laterals)+1+max(nodes.node_id))
+#         laterals['NAME_ID'] = ['lateral_{}'.format(i) for i in range(1, len(laterals) + 1)]
+
+#         #add the laterals to the nodes
+#         final_nodes = pd.concat([nodes, laterals])
+#         final_nodes.reset_index(inplace=True, drop=True)
+#         final_nodes.index = np.arange(1, len(final_nodes)+1)
+
+
+
+#         #edges
+#         laterals_to_basin = gpd.GeoDataFrame(laterals, geometry = gpd.points_from_xy(x_coordinates_laterals, y_coordinates_laterals)) #redo this to exclude the offset 
+#         laterals_to_basin_nodes_ids = pd.merge(nodes, laterals_to_basin, on='geometry').node_id_x
+#         laterals_to_basin_nodes_ids = laterals_to_basin.merge(right = final_nodes,
+#                                                      left_on = 'geometry',
+#                                                      right_on = 'geometry').node_id_y
+
+
+#         edges_laterals = gpd.GeoDataFrame(columns = edges.columns)
+#         edges_laterals['from_node_id'] = laterals['node_id']
+#         edges_laterals['from_node_id_new'] = laterals.node_id
+#         edges_laterals['to_node_id_new'] = laterals_to_basin_nodes_ids
+#         edges_laterals['to_node_id'] = edges_laterals.to_node_id_new #['lateral_{}'.format(i) for i in range(1, len(final_nodes[final_nodes['type'] == 'FlowBoundary']) + 1)]
+
+
+
+#         lines = []
+#         for i in range(len(laterals)):
+#             a = laterals.geometry.iloc[i].coords[0]
+
+
+#             lateral_point = laterals.geometry.iloc[i].coords[0] 
+#             edges_lateral_point = laterals_to_basin.geometry.iloc[i].coords[0]
+#             line = LineString([lateral_point, edges_lateral_point])
+#             lines.append(line)
+
+#         edges_laterals['geometry'] = lines
+#         edges_laterals['node_id'] = np.arange(max(edges.node_id)+1, len(edges_laterals)+1+max(edges.node_id))
+
+#         edges = pd.concat([edges, edges_laterals])   
+#         final_nodes.drop_duplicates(inplace=True, subset = ['NAME_ID'])
+#         edges.drop_duplicates(inplace=True, subset = ['from_node_id', 'to_node_id'])
+#         edges.drop(columns=['from_node_id_new', 'to_node_id_new'], inplace = True)
+#         # edges.set_index('node_id', inplace=True)
+#         edges.edge_type = 'flow'
+        
+#         # link it with the FlowBoundary df
+#         flow_boundary_nodes = final_nodes[final_nodes['NAME_ID'].astype(str).str.startswith('lateral')][['geometry', 'node_id']]
+#         FlowBoundary['geometry'] = flow_boundary_nodes.geometry.values
+#         FlowBoundary['node_id'] = flow_boundary_nodes.node_id.values
+#         # FlowBoundary.set_index('node_id', inplace = True)
+#         # print(flow_boundary_nodes)
+#         return final_nodes, edges, FlowBoundary
+    
+    def group_boundary_conditions(self, boundary_df):
+        """
+        Group boundary conditions from a given DataFrame into static and non-stationary types.
+
+        Parameters:
+        - boundary_df (pd.DataFrame): A DataFrame containing boundary condition data.
+
+        Returns:
+        - flow_boundary_df_static (pd.DataFrame): Static flow boundary conditions.
+        - flow_boundary_df_non_stationary (pd.DataFrame): Non-stationary flow boundary conditions.
+        - level_boundary_df_static (pd.DataFrame): Static water level boundary conditions.
+        - level_boundary_df_non_stationary (pd.DataFrame): Non-stationary water level boundary conditions.
+
+        This function filters the input DataFrame based on the 'quantity' and 'function'
+        columns to separate boundary conditions into different categories.
+        """
+        #filter on the given type of boundary.           
+        flow_boundary_df = boundary_df.loc[boundary_df['quantity'] == 'lateral_discharge']
+        flow_boundary_df_static = flow_boundary_df.loc[flow_boundary_df['function'] == 'constant']
+        flow_boundary_df_dynamic = flow_boundary_df.loc[flow_boundary_df['function'] == 'timeseries']
+        
+        level_boundary_df = boundary_df.loc[boundary_df['quantity'] == 'waterlevelbnd']
+        level_boundary_df_static = level_boundary_df.loc[level_boundary_df['function'] == 'constant']
+        level_boundary_df_dynamic = level_boundary_df.loc[level_boundary_df['function'] == 'timeseries']
+        
+        return flow_boundary_df_static, flow_boundary_df_dynamic, level_boundary_df_static, level_boundary_df_dynamic
     
     
     def find_Terminal(self, nodes):
